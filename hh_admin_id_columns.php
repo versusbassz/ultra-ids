02fea79a2e55f815040218341556a11f0a2b0a66
for-obvio <versus.post@gmail.com>
2013-05-02 00:55:18 +0400

    changed column registration algorithm

--- hh_admin_id_columns.php
+++ hh_admin_id_columns.php
@@ -74,7 +74,7 @@
 
 	function hh_custom_column_add ( $columns ) {
 		$column_id = array( 'hh_id' => __( 'ID' ) );
-		$columns = array_slice( $columns, 0, 1, true ) + $column_id + array_slice( $columns, 1, NULL, true );
+		$columns = array_merge( (array) array_slice( $columns, 0, 1, true ) , $column_id , (array) array_slice( $columns, 1, NULL, true ) );
 		return $columns;
 	}
 
