73226aacfa66200bc5bb31db20bc6bb4ac0a3f0e
versusbassz <versus.post@gmail.com>
2017-02-24 20:44:27 +0300

    Change "Author URI" meta-field

--- hh_admin_id_columns.php
+++ hh_admin_id_columns.php
@@ -5,7 +5,7 @@
 	Description: Sortable ID columns for all standard data types in wordpress admin panel.
 	Version: 1.0.0
 	Author: Vladimir Sklyar
-	Author URI: http://studio.hamburg-hram.de
+	Author URI: http://imgf.ru/
 
 	Minimalistic fork of "Reveal IDs" plugin.
 	*/
