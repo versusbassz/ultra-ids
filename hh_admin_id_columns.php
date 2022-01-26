501ca2b4af6e9fd8c7e323bf502879dbbd2b54d7
Vladimir Sklyar <versus.post@gmail.com>
2022-01-26 21:13:08 +0300

    Refactor the code of the plugin (from the set of functions to the static class)

--- hh_admin_id_columns.php
+++ hh_admin_id_columns.php
@@ -11,74 +11,10 @@ Author URI: https://versusbassz.com/
 Minimalistic fork of "Reveal IDs" plugin.
 */
 
-if (is_admin()) {
-    add_action('admin_init', 'hh_add_custom_columns');
-    add_action('admin_head', 'hh_add_custom_columns_css');
-}
+add_action('plugins_loaded', 'hhid_start_plugin');
 
-function hh_add_custom_columns()
-{
-    add_filter('manage_media_columns', 'hh_custom_column_add');
-    add_action('manage_media_custom_column', 'hh_custom_column_value', 10, 2);
+function hhid_start_plugin() {
+	require __DIR__ . '/src/Plugin.php';
 
-    add_filter('manage_link-manager_columns', 'hh_custom_column_add');
-    add_action('manage_link_custom_column', 'column_value', 10, 2);
-
-    add_action('manage_edit-link-categories_columns', 'hh_custom_column_add');
-    add_filter('manage_link_categories_custom_column', 'column_return_value', 10, 3);
-
-    foreach (get_taxonomies() as $taxonomy) {
-        add_action("manage_edit-{$taxonomy}_columns", 'hh_custom_column_add');
-        add_filter("manage_{$taxonomy}_custom_column", 'hh_custom_column_return_value', 10, 3);
-        add_filter("manage_edit-{$taxonomy}_sortable_columns", 'hh_custom_column_add');
-    }
-
-    foreach (get_post_types() as $ptype) {
-        add_action("manage_edit-{$ptype}_columns", 'hh_custom_column_add');
-        add_filter("manage_{$ptype}_posts_custom_column", 'hh_custom_column_value', 10, 3);
-        add_filter("manage_edit-{$ptype}_sortable_columns", 'hh_custom_column_add');
-    }
-
-    add_action('manage_users_columns', 'hh_custom_column_add');
-    add_filter('manage_users_custom_column', 'hh_custom_column_return_value', 10, 3);
-    add_filter('manage_users_sortable_columns', 'hh_custom_column_add');
-
-    add_action('manage_edit-comments_columns', 'hh_custom_column_add');
-    add_action('manage_comments_custom_column', 'hh_custom_column_value', 10, 2);
-    add_filter('manage_edit-comments_sortable_columns', 'hh_custom_column_add');
-}
-
-function hh_custom_column_add($columns)
-{
-    $column_id = array('hh_id' => __('ID'));
-
-    $columns   = array_merge(
-        (array) array_slice($columns, 0, 1, true),
-        $column_id,
-        (array) array_slice($columns, 1, null, true)
-    );
-
-    return $columns;
-}
-
-function hh_custom_column_value($column_name, $id)
-{
-    if ($column_name === 'hh_id') {
-        echo $id;
-    }
-}
-
-function hh_custom_column_return_value($value, $column_name, $id)
-{
-    if ($column_name === 'hh_id') {
-        $value .= $id;
-    }
-
-    return $value;
-}
-
-function hh_add_custom_columns_css()
-{
-    // 6 chars
-    echo PHP_EOL . '<style type="text/css"> table.widefat th.column-hh_id { width: 65px; } </style>' . PHP_EOL;
+	add_action('init', [\Versusbassz\IdColumns\Plugin::class, 'init']);
 }
---
+++ src/Plugin.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Versusbassz\IdColumns;
+
+class Plugin {
+	const COLUMN_ID = 'hh_id';
+
+	public static function init() {
+		if ( is_admin() ) {
+			add_action( 'admin_init', [ __CLASS__, 'register_columns' ] );
+			add_action( 'admin_head', [ __CLASS__, 'add_css' ] );
+		}
+	}
+
+	public static function register_columns() {
+		add_filter( 'manage_media_columns', [ __CLASS__, 'add_column' ] );
+		add_action( 'manage_media_custom_column', [ __CLASS__, 'echo_value' ], 10, 2 );
+
+		add_filter( 'manage_link-manager_columns', [ __CLASS__, 'add_column' ] );
+		add_action( 'manage_link_custom_column', [ __CLASS__, 'echo_value' ], 10, 2 );
+
+		add_action( 'manage_edit-link-categories_columns', [ __CLASS__, 'add_column' ] );
+		add_filter( 'manage_link_categories_custom_column', [ __CLASS__, 'return_value' ], 10, 3 );
+
+		foreach ( get_taxonomies() as $taxonomy ) {
+			add_action( "manage_edit-{$taxonomy}_columns", [ __CLASS__, 'add_column' ] );
+			add_filter( "manage_{$taxonomy}_custom_column", [ __CLASS__, 'return_value' ], 10, 3 );
+			add_filter( "manage_edit-{$taxonomy}_sortable_columns", [ __CLASS__, 'add_column' ] );
+		}
+
+		foreach ( get_post_types() as $ptype ) {
+			add_action( "manage_edit-{$ptype}_columns", [ __CLASS__, 'add_column' ] );
+			add_filter( "manage_{$ptype}_posts_custom_column", [ __CLASS__, 'echo_value' ], 10, 3 );
+			add_filter( "manage_edit-{$ptype}_sortable_columns", [ __CLASS__, 'add_column' ] );
+		}
+
+		add_action( 'manage_users_columns', [ __CLASS__, 'add_column' ] );
+		add_filter( 'manage_users_custom_column', [ __CLASS__, 'return_value' ], 10, 3 );
+		add_filter( 'manage_users_sortable_columns', [ __CLASS__, 'add_column' ] );
+
+		add_action( 'manage_edit-comments_columns', [ __CLASS__, 'add_column' ] );
+		add_action( 'manage_comments_custom_column', [ __CLASS__, 'echo_value' ], 10, 2 );
+		add_filter( 'manage_edit-comments_sortable_columns', [ __CLASS__, 'add_column' ] );
+	}
+
+	public static function add_css() {
+		// 6 chars
+		echo PHP_EOL . '<style type="text/css"> table.widefat th.column-hh_id { width: 65px; } </style>' . PHP_EOL;
+	}
+
+	public static function add_column( $columns ) {
+		$column_id = [ self::COLUMN_ID => __( 'ID' ) ];
+
+		$columns = array_merge(
+			(array) array_slice( $columns, 0, 1, true ),
+			$column_id,
+			(array) array_slice( $columns, 1, null, true )
+		);
+
+		return $columns;
+	}
+
+	public static function echo_value( $column_name, $id ) {
+		if ( $column_name === self::COLUMN_ID ) {
+			echo $id;
+		}
+	}
+
+	public static function return_value( $value, $column_name, $id ) {
+		if ( $column_name === self::COLUMN_ID ) {
+			$value .= $id;
+		}
+
+		return $value;
+	}
+}
