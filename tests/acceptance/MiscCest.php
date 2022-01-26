7ba510b50a7a21eaeda2c1d0972cfe93ac2ddc44
Vladimir Sklyar <versus.post@gmail.com>
2022-01-26 21:09:36 +0300

    dev: Add e2e-tests for the output (columns) of the plugin

--- tests/acceptance/MiscCest.php
+++ tests/acceptance/MiscCest.php
@@ -1,10 +1,6 @@
 <?php
 
-class MiscCest
-{
-	public function _before( AcceptanceTester $I ) {
-	}
-
+class MiscCest {
 	public function checkFrontendWorks( AcceptanceTester $I ) {
 		$I->amOnPage( '/' );
 		$I->see( 'Welcome to WordPress' );
@@ -13,6 +9,44 @@ class MiscCest
 	public function checkDashboardWorks( AcceptanceTester $I ) {
 		$I->loginAsAdmin();
 		$I->amOnPage( '/wp-admin/index.php' );
-		$I->see( 'At a Glance' );;
+		$I->see( 'At a Glance' );
+
+		$th_selector = '#hh_id';
+		$td_selector = 'td.hh_id';
+
+		// Posts
+		$I->amOnPage( '/wp-admin/edit.php' );
+		$I->see( 'ID', $th_selector );
+		$I->see( '1', $td_selector );
+
+		// Categories
+		$I->amOnPage( '/wp-admin/edit-tags.php?taxonomy=category' );
+		$I->see( 'ID', $th_selector );
+		$I->see( '1', $td_selector );
+
+		// Media
+		$I->amOnPage( '/wp-admin/upload.php?mode=list' );
+		$I->see( 'ID', $th_selector );
+		// TODO upload a file and check a td value
+
+		// Pages
+		$I->amOnPage( '/wp-admin/edit.php?post_type=page' );
+		$I->see( 'ID', $th_selector );
+		$I->see( '2', $td_selector );
+
+		// Links
+		$I->amOnPage( '/wp-admin/link-manager.php' );
+		$I->see( 'ID', $th_selector );
+		// TODO create a link and check a td value
+
+		// Comments
+		$I->amOnPage( '/wp-admin/edit-comments.php' );
+		$I->see( 'ID', $th_selector );
+		$I->see( '1', $td_selector );
+
+		// Users
+		$I->amOnPage( '/wp-admin/users.php' );
+		$I->see( 'ID', $th_selector );
+		$I->see( '1', $td_selector );
 	}
 }
